25fc82b469e8cbb5ac28574b7f28c594d10af845
Anoop Mathew <anoop.mathew@consultant.volvo.com>
2024-01-19 14:25:23 +0100

    pass call backs in aot interface

--- AotLib/Class1.cs
+++ AotLib/Class1.cs
@@ -11,4 +11,18 @@ public static class NativeEntryPoints
         return x + y;
     }
 
+    // [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    // public delegate void CppCallback(int value);
+
+    public static unsafe delegate* unmanaged[Cdecl]<int, void> CppCallback;
+
+    [UnmanagedCallersOnly(EntryPoint = "CallCppDelegate")]
+    public static unsafe void CallCppDelegate(delegate* unmanaged[Cdecl]<int, void> cppCallback)
+    {
+        if (cppCallback != null)
+        {
+            cppCallback(100); // Example argument
+        }
+    }
+
 }
--- testlib.cpp
+++ testlib.cpp
@@ -7,12 +7,26 @@
 
 
 int add(int x, int y);
+void callDelegate();
+
+extern "C" void MyCppFunction(int value)
+{
+    std::cout << "Called from C#: " << value << std::endl;
+}
+
+typedef void (*CallCppDelegateFunction)(void (*)(int));
 
 int main(int argc, char** argv)
 {
+    // std::cout << "Starting up" << std::endl;
+    // auto result = add(2,8);
+    // std::cout << "Sum of 2 and 8 is " << result <<  std::endl;
+
     std::cout << "Starting up" << std::endl;
-    auto result = add(2,8);
-    std::cout << "Sum of 2 and 8 is " << result <<  std::endl;
+    callDelegate();
+    std::cout << "Done with calling the delegate "  <<  std::endl;
+
+    
 }
 
 int add(int x, int y)
@@ -24,4 +38,30 @@ int add(int x, int y)
     std::cout << "Calling add" << std::endl;
     auto result = addFuncHandle(x,y);    
     return result;
+}
+
+void callDelegate(){
+    auto handle =  dlopen("./aotlib.so", RTLD_LAZY);
+    if (!handle) {
+        std::cerr << "Failed to load the library" << std::endl;
+        return;
+    }
+
+    // Get the function address
+    auto callCppDelegate = (CallCppDelegateFunction)dlsym(handle, "CallCppDelegate");
+
+    const char* dlsym_error = dlerror();
+
+    if (dlsym_error) {
+        std::cerr << "Cannot load symbol 'CallCppDelegate': " << dlsym_error << std::endl;
+        dlclose(handle);
+        return;
+    }
+
+    // Call the function with a C++ function as a delegate
+    callCppDelegate(MyCppFunction);
+
+    dlclose(handle);
+    return;
+
 }
