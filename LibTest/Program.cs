35d1c5520632352e5cf7d84f3363685b2383601f
Anoop Mathew <anoop.mathew@consultant.volvo.com>
2024-01-23 10:27:16 +0100

    updated interop with done call back

--- LibTest/Program.cs
+++ LibTest/Program.cs
@@ -2,6 +2,13 @@
 
 namespace LibTest
 {
+
+    public struct ReturnValue{
+        public int success;
+        public int totalSeconds;
+        
+    }
+
     internal class Program
     {
         static void Main(string[] args)
@@ -10,14 +17,28 @@ namespace LibTest
             Console.WriteLine("$Starting up the DLL interop");
 
             var callback = new ProgressCallBackkDelegate(OnProgressChange);
-            var result = longRunningProcess(10,callback);            
+            var doneCallBack = new DoneCallBackDelegate(onDoneCallBack);
+            var result = longRunningProcess(10,callback,doneCallBack);  
+
+            Console.WriteLine("Press any key to exit the app");
+            Console.ReadLine();          
             
         }
+
+        public static void onDoneCallBack(ReturnValue value)
+        {
+            Console.WriteLine($"Swdl done and it is : {(value.success == 1 ? "Successful":"Failure")} and it took {value.totalSeconds} seconds");
+        }
+
 
         public static void OnProgressChange(int value)
         {
             Console.WriteLine($"The progress is : {value} %");
         }
+
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void DoneCallBackDelegate(ReturnValue value);
 
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         public delegate void ProgressCallBackkDelegate(int value);
@@ -27,6 +48,6 @@ namespace LibTest
         public static extern int add(int x, int y);
 
         [DllImport("aotlib.so")]
-        public static extern int longRunningProcess(int x, ProgressCallBackkDelegate  callback);
+        public static extern int longRunningProcess(int x, ProgressCallBackkDelegate  callback, DoneCallBackDelegate doneCallBack);
     }
 }
